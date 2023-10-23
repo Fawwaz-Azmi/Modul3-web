05379d3e960802e21ea94f90b10ff8a3e15ccda5
Fawwaz-Azmi <fawwazgaul@gmail.comi>
2023-10-23 16:33:19 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -15,3 +15,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// const Percobaan = <h1>percobaan ini menggunakan jsx</h1>
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(Percobaan);
